f7a8a12b519eb0b927df0d0c37709f5acac6fd0a
Prasanthi-Peram <prasanthiperam29@gmail.com>
2026-03-08 19:09:53 +0530

    feat: change schema to accommodate ml predictions and alerts

--- api/migrations.sql
+++ api/migrations.sql
@@ -1,13 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE IF NOT EXISTS ac_device (
-    device_id   BIGINT PRIMARY KEY,
-    location    TEXT NOT NULL
+    device_id   BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    location    TEXT NOT NULL,
+    last_service TIMESTAMPTZ  DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS device_telemetry (
     time_stamp        TIMESTAMPTZ NOT NULL,
-    device_id         BIGINT NOT NULL,
+    device_id         BIGINT NOT NULL REFERENCES ac_device(device_id) ON DELETE CASCADE,
 
     current           DOUBLE PRECISION,
     voltage           DOUBLE PRECISION,
@@ -19,13 +22,48 @@ CREATE TABLE IF NOT EXISTS device_telemetry (
     humidity          DOUBLE PRECISION,
 
     unit_consumption  DOUBLE PRECISION,
-    updated_at        TIMESTAMPTZ NOT NULL DEFAULT now()
+    updated_at        TIMESTAMPTZ NOT NULL DEFAULT now(),
+    PRIMARY KEY (time_stamp, device_id)
 );
 
-ALTER TABLE device_telemetry
-ADD CONSTRAINT fk_device_telemetry_ac_device
-FOREIGN KEY (device_id) REFERENCES ac_device(device_id)
-ON DELETE CASCADE;
+SELECT create_hypertable('device_telemetry', 'time_stamp', if_not_exists => TRUE);
+
+CREATE TABLE IF NOT EXISTS alerts(
+    alert_id      BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    time_stamp    TIMESTAMPTZ NOT NULL,
+    device_id     BIGINT NOT NULL,
+
+
+    alert_text    TEXT NOT NULL,
+    is_true_alarm BOOLEAN DEFAULT NULL, 
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT now(),
+    resolved_at   TIMESTAMPTZ,
+     alert_criticality   TEXT CHECK (alert_criticality IN ('Critical', 'Warning', 'Info')),
+    recommendation TEXT,
+
+    predicted_service_date TIMESTAMPTZ DEFAULT NULL,
+    CONSTRAINT fk_telemetry_alerts 
+    FOREIGN KEY (time_stamp, device_id) 
+    REFERENCES device_telemetry (time_stamp, device_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS ml_predictions (
+    prediction_id      BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    time_stamp          TIMESTAMPTZ NOT NULL,
+    device_id           BIGINT NOT NULL,
+    
+    predicted_state     INT,
+    health_score        DOUBLE PRECISION,
+    performance_score   DOUBLE PRECISION,
+   
+    
+    --ground_truth        INT,
+    --is_error            BOOLEAN DEFAULT FALSE,
+    model_version       TEXT,
+    CONSTRAINT fk_telemetry_predictions 
+    FOREIGN KEY (time_stamp, device_id) 
+    REFERENCES device_telemetry (time_stamp, device_id) ON DELETE CASCADE
+);
 
 CREATE TABLE IF NOT EXISTS users (
     id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -34,5 +72,6 @@ CREATE TABLE IF NOT EXISTS users (
     name          VARCHAR(255),
     created_at    TIMESTAMPTZ NOT NULL DEFAULT now()
 );
+
 
 CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);
